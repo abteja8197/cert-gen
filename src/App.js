6615d59b90dc0d9b0da6d23c6190a8237a9a5aed
Bhanu Teja Adapa <bhanuteja.a>
2020-03-13 19:12:45 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header'
+
+import Steppercomponent from './Components/Stepper'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Steppercomponent />
+    </div>
+  );
+}
+
+export default App;
