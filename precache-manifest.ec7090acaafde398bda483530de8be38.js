076e0659a1251c8611d8b2e67842a8d22785f9bd
Bhanu Teja Adapa <bhanuteja.a>
2020-03-13 19:23:50 +0530

    Updates

---
+++ precache-manifest.ec7090acaafde398bda483530de8be38.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c59eb17c9c3412734edad53c1ab3d38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "237b015db82da50657e0",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "a61d2a2556adc24619b7",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "237b015db82da50657e0",
+    "url": "/static/js/2.fbf3b735.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.fbf3b735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61d2a2556adc24619b7",
+    "url": "/static/js/main.19fd249e.chunk.js"
+  },
+  {
+    "revision": "ce6820888dff26d6b35f",
+    "url": "/static/js/runtime-main.bacb566f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
